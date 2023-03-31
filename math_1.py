455195320f4715aaf51a574177da63e8c8cc6b6d
wambui-nyambura <nyamburawambuui@gmail.com>
2023-03-31 16:57:43 +0300

    math files

---
+++ math_1.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    answer= a+b
+    return answer
+def minus(num1,num2):
+    result=num1-num2
+    return result
+def divide(x,y):
+    answer=x/y
+    return answer
+def multiply(x,y):
+    result=x*y
+    return result
+def reminder(x,y):
+    result=x%y
+    return result
